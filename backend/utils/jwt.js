0fad05286726e83762748d92ef1451a4a0ef9922
Denis <123562174+borove4ik@users.noreply.github.com>
2024-01-20 12:06:39 +0300

    jwt

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET, JWT_SECRET_DEVELOPMENT } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-const generateToken = (payload) => jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEVELOPMENT, { expiresIn: '7d' });
+const generateToken = (payload) => jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key', { expiresIn: '7d' });
 
 module.exports = generateToken;
